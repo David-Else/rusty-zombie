7d37529d241b14778405152ce1bfe2f218067543
David-Else <12832280+David-Else@users.noreply.github.com>
2022-09-07 17:54:27 +0100

    New algo is back, still doesn't work

--- src/zombie.rs
+++ src/zombie.rs
@@ -8,21 +8,21 @@ pub struct Zombie {
     image: char,
 }
 
-// pub fn calculate_random_position_around_point(
-//     hero_position: Point2d,
-//     screen_width_height: Point2d,
-// ) -> Point2d {
-//     let variation_in_r = 1;
-//     let minimum_r = screen_width_height.y / 2; // could be x
-//     let theta: f32 = thread_rng().gen::<f32>() * (2.0 * PI);
-//     let r = thread_rng().gen::<i32>() * variation_in_r + minimum_r as i32;
-//     println!("{:?}", r);
-//     Point2d {
-//         // cos and sin need floating point as well as PI
-//         x: theta.cos() as usize * r + hero_position.x,
-//         y: theta.sin() as usize * r + hero_position.y,
-//     }
-// }
+pub fn calculate_random_position_around_point(
+    hero_position: Point2d<u8>,
+    screen_width_height: Point2d<u8>,
+) -> Point2d<u8> {
+    let variation_in_r = 1;
+    let minimum_r = screen_width_height.y / 2; // could be x
+    let theta: f32 = thread_rng().gen::<f32>() * (2.0 * PI);
+    let r = thread_rng().gen::<i32>() * variation_in_r + minimum_r as i32;
+    println!("{:?}", r);
+    Point2d {
+        // cos and sin need floating point as well as PI
+        x: theta.cos() as u8 * r as u8 + hero_position.x,
+        y: theta.sin() as u8 * r as u8 + hero_position.y,
+    }
+}
 
 impl Entity for Zombie {
     // associated (static) function, used as constructor
@@ -46,17 +46,17 @@ impl Entity for Zombie {
         //   ];
         // };
 
-        // let rando = calculate_random_position_around_point(
-        //     Point2d {
-        //         x: screen_size.x / 2,
-        //         y: screen_size.y / 2,
-        //     },
-        //     screen_size,
-        // );
+        let rando = calculate_random_position_around_point(
+            Point2d {
+                x: screen_size.x / 2,
+                y: screen_size.y / 2,
+            },
+            screen_size,
+        );
 
-        // println!("NEW ALGO {:?}", rando);
-        // println!("OLD ALGO {:?}", random_position);
-        // // ---------------------------------------
+        println!("NEW ALGO {:?}", rando);
+        println!("OLD ALGO {:?}", random_position);
+        // ---------------------------------------
         Self {
             position: random_position,
             image,

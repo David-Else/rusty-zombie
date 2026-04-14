eb650756697e541837b0c54bba6347f5d4babb39
David <12832280+David-Else@users.noreply.github.com>
2026-04-14 12:36:47 +0100

    Remove nested if

--- src/input.rs
+++ src/input.rs
@@ -17,19 +17,19 @@ pub struct InputHandler;
 
 impl InputHandler {
     pub fn process_input() -> Option<GameInput> {
-        if event::poll(Duration::from_millis(0)).unwrap() {
-            if let Ok(Event::Key(key_event)) = event::read() {
-                return match key_event.code {
-                    KeyCode::Char('q') => Some(GameInput::Exit),
-                    KeyCode::Char('s') => Some(GameInput::Start),
-                    KeyCode::Char('k') => Some(GameInput::MoveUp),
-                    KeyCode::Char('j') => Some(GameInput::MoveDown),
-                    KeyCode::Char('h') => Some(GameInput::MoveLeft),
-                    KeyCode::Char('l') => Some(GameInput::MoveRight),
-                    KeyCode::Char('f') => Some(GameInput::Fire),
-                    _ => None,
-                };
-            }
+        if event::poll(Duration::from_millis(0)).unwrap()
+            && let Ok(Event::Key(key_event)) = event::read()
+        {
+            return match key_event.code {
+                KeyCode::Char('q') => Some(GameInput::Exit),
+                KeyCode::Char('s') => Some(GameInput::Start),
+                KeyCode::Char('k') => Some(GameInput::MoveUp),
+                KeyCode::Char('j') => Some(GameInput::MoveDown),
+                KeyCode::Char('h') => Some(GameInput::MoveLeft),
+                KeyCode::Char('l') => Some(GameInput::MoveRight),
+                KeyCode::Char('f') => Some(GameInput::Fire),
+                _ => None,
+            };
         }
         None
     }
